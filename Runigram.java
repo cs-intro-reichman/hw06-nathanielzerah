91758d8c4288f5d1502d9ef5ca8be16fe3457c37
nathanielzerah <154419186+nathanielzerah@users.noreply.github.com>
2024-01-27 17:56:54 +0200

    Runigram.java

--- Runigram.java
+++ Runigram.java
@@ -44,8 +44,8 @@ public class Runigram {
 		// For each pixel (i,j), reads 3 values from the file,
 		// creates from the 3 colors a new Color object, and 
 		// makes pixel (i,j) refer to that object.
-		for (int i = 0; i < numCols; i++){
-            for (int j = 0; j < numRows; j++){
+		for (int i = 0; i < numRows; i++){
+            for (int j = 0; j < numCols; j++){
                 int red = in.readInt();
                 int green = in.readInt();
                 int blue = in.readInt();
@@ -124,13 +124,13 @@ public class Runigram {
 	public static Color[][] grayScaled(Color[][] image) {
         int numRows = image.length;
         int numCols = image[0].length;
-        Color[][] grey = new Color[numRows][numCols];
+        Color[][] gray = new Color[numRows][numCols];
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
-                grey[i][j] = luminance(image[i][j]);
+                gray[i][j] = luminance(image[i][j]);
             }
         }
-        return grey;
+        return gray;
 	}	
 	
 	/**
@@ -141,10 +141,10 @@ public class Runigram {
         int numRows = image.length;
         int numCols = image[0].length;
         Color[][] scaled = new Color[width][height];
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                int x = (int)(i * (numRows / height));
-                int y = (int)(j * (numCols / width));
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int x = (int)(i * (numRows /(double) width));
+                int y = (int)(j * (numCols / (double) height));
                 scaled[i][j] = image[x][y];
             }          
         }
@@ -189,7 +189,7 @@ public class Runigram {
 	 * of the source image.
 	 */
 	public static void morph(Color[][] source, Color[][] target, int n) {
-		target = scaled(source, source[0].length, source.length);
+		target = scaled(target, source[0].length, source.length);
         setCanvas(source);
         for (int i = 0; i <= n; i++) {
             double alpha = (double)(n - i) / n;
